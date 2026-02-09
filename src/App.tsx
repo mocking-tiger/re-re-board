67e761706670934fcfde5e573d22e4ed6c2fb764
ParkJunYong <dyden119@gmail.com>
2026-02-09 21:13:17 +0900

    style: 카드 디자인 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import { useState } from 'react';
 import './App.css';
+import Card from './components/Card/Card';
+import { useState } from 'react';
 import { formatDate, generateId } from './utils/helpers';
 
 const App = () => {
   const [id, setId] = useState('id');
   const [formattedDate, setFormattedDate] = useState('date');
 
+  const cardData = {
+    id: '1',
+    title: '무거운 몸을 또 일으켜 세워',
+    description:
+      '난 부숴지는게 무섭지 않아 힘들땐 잠시만 갓길에 세워 졸음쉼터는 찾으면 가까이 있어난 부숴지는게 무섭지 않아 힘들땐 잠시만 갓길에 세워 졸음쉼터는 찾으면 가까이 있어난 부숴지는게 무섭지 않아 힘들땐 잠시만 갓길에 세워 졸음쉼터는 찾으면 가까이 있어난 부숴지는게 무섭지 않아 힘들땐 잠시만 갓길에 세워 졸음쉼터는 찾으면 가까이 있어',
+    order: 1,
+    createdAt: new Date(),
+  };
+
   return (
     <div className="App">
-      <p>{id}</p>
-      <p>{formattedDate}</p>
       <header className="App-header">
+        <p>{id}</p>
         <button onClick={() => setId(generateId())}>generate id</button>
+        <p>{formattedDate}</p>
         <button onClick={() => setFormattedDate(formatDate(new Date()))}>generate date</button>
       </header>
+      <Card card={cardData} />
     </div>
   );
 };
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,17 @@
+import './Card.css';
+import { Card as CardType } from '../../types';
+import { formatDate } from '../../utils/helpers';
+
+const Card = ({ card }: { card: CardType }) => {
+  console.log(card);
+
+  return (
+    <div className="card">
+      <h1 className="card-title">{card.title}</h1>
+      <p className="card-description">{card.description}</p>
+      <span className="card-date">{formatDate(card.createdAt)}</span>
+    </div>
+  );
+};
+
+export default Card;

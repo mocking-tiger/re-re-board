12421e6d8e79c928a114b3143364ce01c29e5dd2
ParkJunYong <dyden119@gmail.com>
2026-02-12 12:02:50 +0900

    docs: index => types, 더미데이터 이동

---
+++ src/types/types.ts
@@ -0,0 +1,96 @@
+import { generateId } from '../utils/helpers';
+
+// ================================================================
+// 타입, 인터페이스
+// ================================================================
+export interface Card {
+  id: string;
+  title: string;
+  description: string;
+  displayOrder: number;
+  createdAt: Date;
+}
+
+export interface List {
+  id: string;
+  title: string;
+  cards: Card[];
+  displayOrder: number;
+}
+
+export interface Board {
+  id: string;
+  title: string;
+  lists: List[];
+}
+
+// ================================================================
+// 더미데이터
+// ================================================================
+export const cards1 = [
+  {
+    id: generateId(),
+    title: '봄이 오면 산에 들에 진달래 피네',
+    description:
+      '진달래 피는곳에 내 마음도 피어 건너마을 젊은 처자 꽃따러 오면은 꽃만 말고 이 마음도 함께 따가주',
+    displayOrder: 1,
+    createdAt: new Date(),
+  },
+  {
+    id: generateId(),
+    title: '오늘밤 그 말만은 말아요',
+    description:
+      '그냥 서럽고 서러워 난 사연이 많아 추억이 많아 가슴 찢어져요 아직은 안녕 우린 안돼요 넌 그 입을 더 열지마 안녕이라고 내게 말하지마',
+    displayOrder: 2,
+    createdAt: new Date(),
+  },
+  {
+    id: generateId(),
+    title: '밥도 잘 먹지 못해 ',
+    description:
+      '혼자 살아서 뭐해 네가 더 잘 알잖아 너 없인 나 안돼잖아 너밖에 몰라서 너없인 밥도 잘 먹지 못하는 난데 차마 죽지도 못해 네가 돌아올까봐 울면서 날 찾을까봐 오늘도 내일도',
+    displayOrder: 3,
+    createdAt: new Date(),
+  },
+  {
+    id: generateId(),
+    title: '밥도 잘 먹지 못해 ',
+    description:
+      '혼자 살아서 뭐해 네가 더 잘 알잖아 너 없인 나 안돼잖아 너밖에 몰라서 너없인 밥도 잘 먹지 못하는 난데 차마 죽지도 못해 네가 돌아올까봐 울면서 날 찾을까봐 오늘도 내일도',
+    displayOrder: 3,
+    createdAt: new Date(),
+  },
+  {
+    id: generateId(),
+    title: '밥도 잘 먹지 못해 ',
+    description:
+      '혼자 살아서 뭐해 네가 더 잘 알잖아 너 없인 나 안돼잖아 너밖에 몰라서 너없인 밥도 잘 먹지 못하는 난데 차마 죽지도 못해 네가 돌아올까봐 울면서 날 찾을까봐 오늘도 내일도',
+    displayOrder: 3,
+    createdAt: new Date(),
+  },
+  {
+    id: generateId(),
+    title: '밥도 잘 먹지 못해 ',
+    description:
+      '혼자 살아서 뭐해 네가 더 잘 알잖아 너 없인 나 안돼잖아 너밖에 몰라서 너없인 밥도 잘 먹지 못하는 난데 차마 죽지도 못해 네가 돌아올까봐 울면서 날 찾을까봐 오늘도 내일도',
+    displayOrder: 3,
+    createdAt: new Date(),
+  },
+  {
+    id: generateId(),
+    title: '밥도 잘 먹지 못해 ',
+    description:
+      '혼자 살아서 뭐해 네가 더 잘 알잖아 너 없인 나 안돼잖아 너밖에 몰라서 너없인 밥도 잘 먹지 못하는 난데 차마 죽지도 못해 네가 돌아올까봐 울면서 날 찾을까봐 오늘도 내일도',
+    displayOrder: 3,
+    createdAt: new Date(),
+  },
+];
+
+export const lists = [
+  {
+    id: generateId(),
+    title: '오늘 할 일',
+    cards: cards1,
+    displayOrder: 1,
+  },
+];

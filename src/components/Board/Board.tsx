cd9b7385f0979d4d5e5b278bda8401da5b521bf1
ParkJunYong <dyden119@gmail.com>
2026-02-24 14:10:23 +0900

    refactor: useState => useReducer 변경

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,88 +1,17 @@
 import './Board.css';
 import List from '../List/List';
 import Button from '../UI/Button/Button';
-import { useState } from 'react';
-import { generateId, getNextDisplayOrder } from '../../utils/helpers';
+import { useEffect, useReducer } from 'react';
 import { getBoard, initialBoard, saveBoard } from '../../utils/storage';
-import { Board as BoardType, List as ListType, Card as CardType } from '../../types/types';
+import { boardReducer } from '../../reducers/boardReducer';
 
 const Board = () => {
-  const [board, setBoard] = useState<BoardType | null>(() => getBoard() || initialBoard());
+  const [board, dispatch] = useReducer(boardReducer, null, () => getBoard() || initialBoard());
 
-  const addList = () => {
-    if (!board) return;
+  useEffect(() => {
+    saveBoard(board);
+  }, [board]);
 
-    const newList: ListType = {
-      id: generateId(),
-      title: '새 리스트',
-      cards: [],
-      displayOrder: getNextDisplayOrder(board.lists),
-      boardId: board.id,
-    };
-
-    const newBoard = {
-      ...board,
-      lists: [...board.lists, newList],
-    };
-    setBoard(newBoard);
-    saveBoard(newBoard);
-  };
-
-  const deleteList = (listId: string) => {
-    if (!board) return;
-
-    const newBoard = {
-      ...board,
-      lists: board.lists.filter((list) => list.id !== listId),
-    };
-    setBoard(newBoard);
-    saveBoard(newBoard);
-  };
-
-  const addCard = (listId: string) => {
-    if (!board) return;
-
-    const newCard: CardType = {
-      id: generateId(),
-      title: '새 카드',
-      description: '내용을 입력하세요',
-      displayOrder: getNextDisplayOrder(
-        board.lists.find((list) => list.id === listId)?.cards || []
-      ),
-      createdAt: new Date(),
-      listId,
-    };
-
-    const newBoard = {
-      ...board,
-      lists: board.lists.map((list) =>
-        list.id === listId ? { ...list, cards: [...list.cards, newCard] } : list
-      ),
-    };
-    setBoard(newBoard);
-    saveBoard(newBoard);
-  };
-
-  const deleteCard = (listId: string, cardId: string) => {
-    if (!board) return;
-
-    const newBoard = {
-      ...board,
-      lists: board.lists.map((list) =>
-        list.id === listId
-          ? { ...list, cards: list.cards.filter((card) => card.id !== cardId) }
-          : list
-      ),
-    };
-    setBoard(newBoard);
-    saveBoard(newBoard);
-  };
-
-  if (!board) {
-    return <div>보드를 불러오는 중...</div>;
-  }
-
-  console.log(board);
   return (
     <div className="board">
       <h1 className="board-title">{board.title}</h1>
@@ -91,12 +20,14 @@ const Board = () => {
           <List
             key={list.id}
             list={list}
-            onDeleteList={deleteList}
-            onAddCard={addCard}
-            onDeleteCard={deleteCard}
+            onDeleteList={() => dispatch({ type: 'DELETE_LIST', payload: { listId: list.id } })}
+            onAddCard={() => dispatch({ type: 'ADD_CARD', payload: { listId: list.id } })}
+            onDeleteCard={(listId, cardId) =>
+              dispatch({ type: 'DELETE_CARD', payload: { listId, cardId } })
+            }
           />
         ))}
-        <Button className="board-add-list-button" onClick={addList}>
+        <Button className="board-add-list-button" onClick={() => dispatch({ type: 'ADD_LIST' })}>
           +
         </Button>
       </div>
---
+++ src/reducers/boardReducer.ts
@@ -0,0 +1,70 @@
+import { Board, Card, List } from '../types/types';
+import { generateId, getNextDisplayOrder } from '../utils/helpers';
+
+export type BoardAction =
+  | { type: 'ADD_LIST' }
+  | { type: 'DELETE_LIST'; payload: { listId: string } }
+  | { type: 'ADD_CARD'; payload: { listId: string } }
+  | { type: 'DELETE_CARD'; payload: { listId: string; cardId: string } };
+
+export const boardReducer = (state: Board, action: BoardAction) => {
+  console.log(state);
+  console.log(action);
+
+  switch (action.type) {
+    case 'ADD_LIST':
+      const newList: List = {
+        id: generateId(),
+        title: '새 리스트',
+        cards: [],
+        displayOrder: getNextDisplayOrder(state.lists),
+        boardId: state.id,
+      };
+      return {
+        ...state,
+        lists: [...state.lists, newList],
+      };
+
+    case 'DELETE_LIST':
+      return {
+        ...state,
+        lists: state.lists.filter((list) => list.id !== action.payload.listId),
+      };
+
+    case 'ADD_CARD':
+      const newCard: Card = {
+        id: generateId(),
+        title: '새 카드',
+        description: '내용을 입력하세요',
+        displayOrder: getNextDisplayOrder(
+          state.lists.find((list) => list.id === action.payload.listId)?.cards || []
+        ),
+        createdAt: new Date(),
+        listId: action.payload.listId,
+      };
+
+      return {
+        ...state,
+        lists: state.lists.map((list) =>
+          list.id === action.payload.listId
+            ? {
+                ...list,
+                cards: [...list.cards, newCard],
+              }
+            : list
+        ),
+      };
+
+    case 'DELETE_CARD':
+      return {
+        ...state,
+        lists: state.lists.map((list) =>
+          list.id === action.payload.listId
+            ? { ...list, cards: list.cards.filter((card) => card.id !== action.payload.cardId) }
+            : list
+        ),
+      };
+    default:
+      return state;
+  }
+};
